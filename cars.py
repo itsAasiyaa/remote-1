4bc866610e19f85db9e3c8dfebd092f70f4eb3b8
Bibi Aasiyaa Jaulim <60102393@udst.edu.qa>
2024-02-01 21:55:30 +0300

    Third commit, cars.py updated to print a randomly selected model year from modelYear() function

--- cars.py
+++ cars.py
@@ -1,5 +1,10 @@
 import random
 
+def modelYear():
+    year = [2021, 2022, 2023, 2024]
+    return random.choice(year)
+
 print("This is file cars.py")
 cars = ["GMC", "Audi", "Mercedes-Benz", "Lexus"]
 print("Randomly selected car:",random.choice(cars))
+print("Randomly selected model year:",modelYear())
